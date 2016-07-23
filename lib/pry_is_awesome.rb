fd5516dc3c347ded1066f35e96d613052703a4aa
hkim53 <paragon.hankido@gmail.com>
2016-07-23 13:39:33 -0400

    Done.

--- lib/pry_is_awesome.rb
+++ lib/pry_is_awesome.rb
@@ -1,12 +1,5 @@
-require 'pry'
 
-def prying_into_the_method
-	inside_the_method = "We're inside the method"
-	puts inside_the_method
-	puts "We're about to stop because of pry!"
-	binding.pry
-	this_variable_hasnt_been_interpreted_yet = "The program froze before it could read me!" 
-	puts this_variable_hasnt_been_interpreted_yet
+def plus_two(num = 3)
+  sum = num + 2
+  puts sum
 end
-
-prying_into_the_method
